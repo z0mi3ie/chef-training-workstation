7542e6b0df9d779b2263695e771d475a6569ac40
ChefDK User <chef@ip-172-31-60-184.ec2.internal>
2015-07-23 15:42:07 +0000

    Fleshed out the workstation tests

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -3,7 +3,26 @@ require 'spec_helper'
 describe 'workstation::default' do
   # Serverspec examples can be found at
   # http://serverspec.org/resource_types.html
+  
   describe package('tree') do
     it { should be_installed }
   end
+
+  describe package('vim') do
+    it { should be_installed }
+  end
+  
+  describe package('git') do
+    it { should be_installed }
+  end
+
+  describe file('/etc/motd') do
+    it { should be_file } 
+    it { should exist }
+    it { should be_owned_by "root" }
+    its(:content) { should match /Property of Chef/ }
+  end
+
+
+  
 end
