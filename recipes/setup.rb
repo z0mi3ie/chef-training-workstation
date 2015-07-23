e0213a84b29e5ec7789a5b688963dee1fd2411ae
ChefDK User <chef@ip-172-31-60-184.ec2.internal>
2015-07-23 19:14:45 +0000

    Version 0.2.1 - Templated /etc/motd

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -10,17 +10,10 @@ package 'git' do
   action :install
 end
 
-file '/etc/motd' do
-  content "Property of Chef. Welcome! 
-  
-  IPADDRESS: #{node["ipaddress"]}
-  HOSTNAME : #{node["hostname"]}
-  MEMORY   : #{node["memory"]["total"]}
-  CPU      : #{node["cpu"]["0"]["mhz"]}
-
-  " 
-  action :create
+template '/etc/motd' do
+  source 'motd.erb'
 end
+
 
 
 
